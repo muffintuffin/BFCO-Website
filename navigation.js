5b3ea1ed59825b596f5dffd3af56022fc6607a7c
David <45515409+muffintuffin@users.noreply.github.com>
2022-11-16 10:08:55 -0500

    added initial files

---
+++ navigation.js
@@ -0,0 +1,72 @@
+/*
+     Author:    Deanna M. Wilborne
+    Created:    2022-11-08
+    Purpose:    Virtualize the Navigation Bar simplifying changes
+     Course:    2022 Fall, CSC186 Intro to Web Development; Sections 10129, 10130
+
+    History:
+                2022-11-08, DMW, this code allows the navigation bar to be created dynamical for all pages
+                2022-11-08, DMW, created.
+*/
+
+'use strict'; // require variables to be declared before use
+
+function setup_navigation() {
+    const nav_html =
+        `<nav class="navbar navbar-expand-lg bg-light position-fixed">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><span id="span-navbar-brand"></span></a>
+                <button class="btn btn-warning ml-auto px-3 order-lg-last" id="button-nav-bar" hidden></button>
+                <a href="" class="ml-auto px-3 order-lg-last no_ul" id="a-phone-link" hidden><span id="span-phone-number"></span></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent"> <!-- collapsing menu items -->
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item list-unstyled">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item list-unstyled">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <!--
+                        <li class="nav-item dropdown list-unstyled">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                               aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled">Disabled</a>
+                        </li>
+                        -->
+                    </ul>
+    
+                    <!--                <form class="d-flex" role="search">-->
+                    <!--                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">-->
+                    <!--                    <button class="btn btn-outline-success" type="submit">Search</button>-->
+                    <!--                </form>-->
+                </div> <!-- end of collapsing menu items -->
+            </div>
+        </nav>
+        `;
+
+    const header = document.getElementById("header");
+    header.innerHTML = nav_html;
+}
+
+function nav_startup() {
+    setup_navigation();
+}
+
+// nav_startup();
---
+++ website.js
@@ -0,0 +1,78 @@
+/*
+     Author:
+    Created:    2022-10-26
+    Purpose:    Template website.js for simple websites
+     Course:    2022 Fall, CSC186 Intro to Web Development; Sections 10129, 10130
+
+    History:
+                2022-11-06, DMW, added copyright and address in footer; phone number in navigation bar
+                2022-10-26, DMW, created.
+*/
+
+'use strict'; // require variables to be declared before use
+
+// ------------------------------------------------------------------------ SITE SPECIFIC INFORMATION
+
+const PHONE_NUMBER = "555-111-2222"; // the organization's phone number; leave blank to exclude from web page
+const PHONE_LINK_TEXT = "5551112222"; // this should have no dashes in it
+const ORG_NAME = "ORGANIZATION NAME"; // put the organization's name here, leave blank to not show
+const ORG_ADDRESS = "CITY, STATE, ZIPCODE"; // the organization's address, leave blank to not show
+const COPYRIGHT_BEGIN_YEAR = ""; // leave empty for only the current year
+const NAV_BAR_BUTTON_TEXT = "BUTTON"; // leave blank for no button
+const APP_BRAND = "BRAND"; // set the brand of your website
+const APP_TITLE = `${APP_BRAND}: MY APP`; // the name of your website or application
+
+// ------------------------------------------------------------------------ END OF SITE SPECIFIC INFORMATION
+
+// set the copyright information in the footer
+function set_copyright() {
+    const SPAN_COPYRIGHT = document.getElementById("span-copyright");
+    let year = new Date().getFullYear();
+    SPAN_COPYRIGHT.innerHTML = "&copy; " +
+        (COPYRIGHT_BEGIN_YEAR !== "" ? `${COPYRIGHT_BEGIN_YEAR}-` : "") +
+        `${year} ${ORG_NAME}`;
+}
+
+// set up a phone number on the navigation bar
+function set_phone_link() {
+    if (PHONE_NUMBER !== "") {
+        const A_PHONE_LINK = document.getElementById("a-phone-link");
+        A_PHONE_LINK.href=`tel:${PHONE_LINK_TEXT}`;
+        A_PHONE_LINK.hidden = false;
+
+        const SPAN_PHONE_NUMBER = document.getElementById("span-phone-number");
+        SPAN_PHONE_NUMBER.innerHTML = PHONE_NUMBER;
+    }
+}
+
+function set_address() {
+    const SPAN_ADDRESS = document.getElementById("span-address");
+    SPAN_ADDRESS.innerHTML = ORG_ADDRESS;
+}
+
+function set_title() {
+    document.getElementById("app-title").innerHTML = APP_TITLE;
+}
+
+function setup_navbar() {
+    document.getElementById("span-navbar-brand").innerHTML = APP_BRAND;
+    if (NAV_BAR_BUTTON_TEXT !== "") {
+        console.log("test");
+        const BUTTON_NAV_BAR = document.getElementById("button-nav-bar");
+        BUTTON_NAV_BAR.innerText = NAV_BAR_BUTTON_TEXT;
+        BUTTON_NAV_BAR.hidden = false;
+    }
+}
+
+// use the startup function to initialize your website
+function startup() {
+    nav_startup();
+    setup_navbar();
+    set_copyright();
+    set_address();
+    set_phone_link();
+    set_title();
+}
+
+// execute the startup function
+startup();
