f10ee8f9233c7d14cee248d26e016662d1049655
David <45515409+muffintuffin@users.noreply.github.com>
2022-11-30 11:01:33 -0500

    Changes 11-30-22: * added logo to navbar. * converted logo from a gif to png and removed background. * Centered navbar items * Removed blue square background

--- navigation.js
+++ navigation.js
@@ -14,7 +14,7 @@
 function setup_navigation() {
     const nav_html =
         `<nav class="navbar navbar-expand-lg bg-light position-fixed">
-            <div class="container-fluid">
+            <div class="container-fluid ">
                 <a class="navbar-brand" href="#"><span id="span-navbar-brand"></span></a>
                 <button class="btn btn-warning ml-auto px-3 order-lg-last" id="button-nav-bar" hidden></button>
                 <a href="" class="ml-auto px-3 order-lg-last no_ul" id="a-phone-link" hidden><span id="span-phone-number"></span></a>
@@ -35,10 +35,11 @@ function setup_navigation() {
                             <a class="nav-link" href="buurr.html">Utility/Rent Assistance</a>
                         </li>
                         <!-- place image in the middle of the nav bar -->
-                        
-                        
-                        
-                        
+                        <a class="navbar-brand" href="#">
+                            <div class="logo-image">
+                            <img src="images/BFCOLogo.png" alt="BCFO Logo" width="100%" height="100%">
+                            </div>
+                        </a>        
                          <li class="nav-item list-unstyled">
                             <a class="nav-link" href="support.html">Support Us</a>
                         </li>
@@ -49,31 +50,10 @@ function setup_navigation() {
                             <a class="nav-link" href="food.html">Food Bank</a>
                         </li>
                         
-                        <!--
-                        <li class="nav-item dropdown list-unstyled">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                               aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                        </li>
-                        -->
+                        
                     </ul>
     
-                    <!--                <form class="d-flex" role="search">-->
-                    <!--                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">-->
-                    <!--                    <button class="btn btn-outline-success" type="submit">Search</button>-->
-                    <!--                </form>-->
+                   
                 </div> <!-- end of collapsing menu items -->
             </div>
         </nav>
