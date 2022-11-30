2c3f263bdb0c318a3487356cb971d35351bd9b6f
David <45515409+muffintuffin@users.noreply.github.com>
2022-11-30 14:43:06 -0500

    added code for slideshow of pictures, current bugs include images not resizing to being equal size, if fade is turned on the image will show a blue screen, and slideshow not autoplaying.

--- website.js
+++ website.js
@@ -37,12 +37,45 @@ function set_copyright() {
 function set_phone_link() {
     if (PHONE_NUMBER !== "") {
         const A_PHONE_LINK = document.getElementById("a-phone-link");
-        A_PHONE_LINK.href=`tel:${PHONE_LINK_TEXT}`;
+        A_PHONE_LINK.href = `tel:${PHONE_LINK_TEXT}`;
         A_PHONE_LINK.hidden = false;
 
         const SPAN_PHONE_NUMBER = document.getElementById("span-phone-number");
         SPAN_PHONE_NUMBER.innerHTML = PHONE_NUMBER;
     }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
 function set_address() {
